c42390c2acdb3cd1dad2325a09fc976a692d13c2
yuldoshov.shakhzod <shakhzodbek.yuldoshev@gmail.com>
2020-06-18 18:39:34 +0500

    168. Posting data to the server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -9,11 +10,23 @@ class NewPost extends Component {
         author: 'Max'
     }
 
+    postDataHandler = () =>{
+        const  data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        }
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+            .then(response => {
+                console.log(response)
+            });
+    }
+
     render () {
         return (
             <div className="NewPost">
                 <h1>Add a Post</h1>
-                <label>Title</label>
+                <label>Title</label>  
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
                 <label>Content</label>
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
@@ -22,7 +35,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
